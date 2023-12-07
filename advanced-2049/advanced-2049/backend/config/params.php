f299fb61e92844b23637d8b2ecdd5f02151c4a44
Daniel Jesus Cahum Chay <l20070006@valladolid.tecnm.mx>
2023-12-07 11:18:43 -0600

    eliminar

--- advanced-2049/advanced-2049/backend/config/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'bsVersion' => '5.x', // Esto establecerá globalmente `bsVersion` en Bootstrap 5.x para todas las extensiones de Krajee
-    // Otras configuraciones globales
-    'adminEmail' => 'admin@example.com',
-];

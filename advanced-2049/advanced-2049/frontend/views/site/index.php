7eb56c7c87157be3c6725fad3514638f46048803
Santos Dzib <l20070029@valladolid.tecnm.mx>
2023-12-07 14:20:45 -0600

    Santos Hizo un cambio

--- advanced-2049/advanced-2049/frontend/views/site/index.php
+++ advanced-2049/advanced-2049/frontend/views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+            <h1 class="display-4">FELICIDADES!</h1>
+            <p class="fs-5 fw-light">Te encuentras ahora mismo en el frontend</p>
+            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Aprende mas de yii2</a></p>
         </div>
     </div>
 
